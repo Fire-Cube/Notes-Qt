ef3b1926a08a2ceb53fe03a7d049fbb1c84ae474
Fire-Cube <ben7@gmx.ch>
2023-08-26 16:31:25 +0200

    fix linguist script

--- update_translation.bat
+++ update_translation.bat
@@ -1,6 +1,6 @@
 cd src
 
-call pyside6-lupdate main.py .\designer\MainWindow.ui ..\designer\MainWindow.ui ..\designer\resources\main_window_resources.qrc-ts .\translations\main_window_de.ts
+call pyside6-lupdate main.py .\designer\MainWindow.ui  .\designer\resources\main_window_resources.qrc -ts .\translations\main_window_de.ts
 call pyside6-lupdate main.py .\designer\ImageSettingsDialog.ui -ts .\translations\image_settings_dialog_de.ts
 
 cd ..
